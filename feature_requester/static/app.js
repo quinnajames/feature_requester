a2113bfa19dcd5526e447399877daee851e43ce7
Quinn James <quinn@quinnjames.net>
2018-01-14 03:03:57 -0500

    Add client based dynamic css

--- feature_requester/static/app.js
+++ feature_requester/static/app.js
@@ -8,6 +8,11 @@ Feature = function(title, description, client, client_priority,
   self.target_date = ko.observable(target_date);
   self.product_area = ko.observable(product_area);
   self.id = id;
+  self.clientClass = ko.computed(function() {
+    let css = self.client().toLowerCase().replace(/ /g,'');
+    console.log(css);
+    return css;
+  })
 }
 
 FeatureViewModel = function() {
@@ -20,6 +25,7 @@ FeatureViewModel = function() {
 
   self.showForm = ko.observable();
   self.showForm(false);
+
 
   function populateNewFeature() {
     return new Feature("", "", "", self.features().length + 1,
@@ -39,4 +45,6 @@ fvm.features.push(new Feature("Feature", "A really important feature",
 "Client A", 1, "4/1/2019", "Business", 2));
 fvm.features.push(new Feature("Feature 2", "Perhaps a bit less important",
 "Client A", 2, "6/17/2019", "Business", 1));
+fvm.features.push(new Feature("New car", "Just literally a new car",
+"Client B", 1, "6/17/2019", "Business", 3));
 ko.applyBindings(fvm);
