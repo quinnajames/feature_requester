798eda58e018a5fd96fea0bc41187d9518eacad5
Quinn James <quinn@quinnjames.net>
2018-01-24 13:45:40 -0500

    Rename updateFeatures to loadFeatures

--- feature_requester/static/app.js
+++ feature_requester/static/app.js
@@ -100,7 +100,7 @@ FeatureViewModel = function() {
     return featureModels;
   }
 
-  self.updateFeatures = function() {
+  self.loadFeatures = function() {
     $.getJSON('/features', (input) => {
       self.features(self.mapJSONToFeatures(input));
     });
@@ -193,7 +193,7 @@ FeatureViewModel = function() {
   }
 
   // on load
-  self.updateFeatures();
+  self.loadFeatures();
   console.log(self.features());
   self.features = self.sortFeatures(self.features);
   console.log('self.features:')
